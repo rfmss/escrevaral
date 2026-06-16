da75e348a5d1af5146e4e6dd8f07881fad12b234
rafa mass <rafamass@rm>
2026-06-16 12:56:02 -0300

    Reordena Bancada por intencao

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "vereda-offline-v504";
-const ASSET_VERSION = "20260617-bancada-semantica";
+const CACHE_NAME = "vereda-offline-v505";
+const ASSET_VERSION = "20260617-bancada-intencao";
 
 const CORE_ASSETS = [
   "./",
