6d93dd72bf1fb6ecaafd402997ede2453ebdba23
Rafa Mass <rafamass@proton.me>
2026-08-01 18:07:01 -0300

    style: carrega a segunda passada reversível do verniz

--- mass-notes-next/src/main.tsx
+++ mass-notes-next/src/main.tsx
@@ -21,7 +21,7 @@ import './styles/m1-usability.css'
 import './styles/pagination.css'
 import './styles/pagination-controls.css'
 import './styles/m1-density.css'
-import './styles/theme-escrevaral-verniz.css'
+import './styles/theme-escrevaral-verniz-material.css'
 
 function ApplicationAccessibilityBridge() {
   useEffect(() => {
