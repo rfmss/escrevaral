c770f3f5efea833cb33eee09a0d4804ee454452b
Rafa Mass <rafamass@proton.me>
2026-09-24 01:14:41 -0300

    Mantém a frente da gaveta acima dos papéis

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "scrvrl-offline-v6-9";
-const ASSET_VERSION = "20260923-scrvrl-cadernos-camadas-v6-9";
+const CACHE_NAME = "scrvrl-offline-v6-10";
+const ASSET_VERSION = "20260924-scrvrl-gaveta-frente-v6-10";
 
 const CORE_ASSETS = [
   "./",
